76d6b743108324febc3505e256d39ca5ffe34112
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2023-06-14 12:07:21 -0300

    first commit

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,15 @@
+export default function initMenuMobile(){
+    
+}
+
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+const eventos = ['click', 'touchstart']
+
+function openMenu(event){
+    menuList.classList.toggle('active');
+    menuButton.classList.toggle('active');
+}
+
+menuButton.addEventListener('click', openMenu)
+
---
+++ js/modules/tooltip.js
@@ -0,0 +1,42 @@
+export default function initToolTip(){
+   
+}
+
+const toolTips = document.querySelectorAll('[data-tooltip]');
+
+toolTips.forEach((item)=>{
+    item.addEventListener('mouseover', onMouseOver)
+})
+
+function onMouseOver(event){
+   const tooltipBox = criarTooltipBox(this)
+
+
+   onMouseLeave.tooltipBox = tooltipBox; 
+   onMouseMove.tooltipBox = tooltipBox;
+   this.addEventListener('mouseleave', onMouseLeave)
+   this.addEventListener('mousemove', onMouseMove)
+}
+
+const onMouseLeave = {
+    tooltipBox: '',
+    handleEvent(){
+        this.tooltipBox.remove()
+    }
+}
+
+const onMouseMove = {
+    handleEvent(event){
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+}
+
+function criarTooltipBox(element){
+    const tooltipBox = document.createElement('div')
+    const text = element.getAttribute('aria-label')
+    tooltipBox.classList.add('tooltip')
+    tooltipBox.innerText = text
+    document.body.appendChild(tooltipBox)
+    return tooltipBox
+}
